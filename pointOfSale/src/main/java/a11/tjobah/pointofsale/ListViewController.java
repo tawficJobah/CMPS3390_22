48070749865c8739488f05a5ddfe7cf854d72024
Tawfic <tawfic661@gmail.com>
2022-05-14 00:59:55 -0700

    add,edit,delete work -save as json file -fix weird stylesheet bug

--- pointOfSale/src/main/java/a11/tjobah/pointofsale/ListViewController.java
+++ pointOfSale/src/main/java/a11/tjobah/pointofsale/ListViewController.java
@@ -4,6 +4,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class ListViewController {
     ObservableList<Item> items = FXCollections.observableArrayList();
@@ -11,13 +13,61 @@ public class ListViewController {
     ListView<Item> itemsList;
 
     @FXML
+    GridPane newItem;
+
+    @FXML
+    TextField txtProductName,txtUPC,txtPrice,txtCost,txtQuantity,txtCategory;
+
+    @FXML
     public void initialize(){
-        Item tmp = new Item("Pepsi","0239482394","1.99","1.05","10","drinks");
-
-        items.add(tmp);
         itemsList.setItems(items);
+        setNewItemVis(false);
 
-        System.out.println(tmp);
     }
 
+    @FXML
+    protected void onNewItem(){
+        setNewItemVis(true);
+    }
+
+    @FXML
+    protected void onSaveItem(){
+        Item tmp = new Item(txtProductName.getText(),txtUPC.getText(),txtPrice.getText(),
+                txtCost.getText(),txtQuantity.getText(),txtCategory.getText());
+        items.add(tmp);
+        setNewItemVis(false);
+        txtProductName.setText(null);
+        txtUPC.setText(null);
+        txtPrice.setText(null);
+        txtCost.setText(null);
+        txtQuantity.setText(null);
+        txtCategory.setText(null);
+    }
+
+    @FXML
+    protected void onEditItem(){
+        Item selectedItem = itemsList.getSelectionModel().getSelectedItem();
+        if(selectedItem != null){
+            onNewItem();
+            txtProductName.setText(selectedItem.getProductName());
+            txtUPC.setText(selectedItem.getUpcCode());
+            txtPrice.setText(selectedItem.getPrice());
+            txtCost.setText(selectedItem.getCost());
+            txtQuantity.setText(selectedItem.getQuantity());
+            txtCategory.setText(selectedItem.getCategory());
+            onDeleteItem();
+        }
+    }
+    @FXML
+    protected void onDeleteItem(){
+        Item selectedItem = itemsList.getSelectionModel().getSelectedItem();
+        if(selectedItem != null){
+            items.remove(selectedItem);
+        }
+    }
+
+    private void setNewItemVis(Boolean vis){
+        newItem.setVisible(vis);
+        newItem.setManaged(vis);
+    }
 }
